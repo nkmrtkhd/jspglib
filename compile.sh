2ad37ce2971015e82eb0389d9478d4d9a91926ff
NAKAMURA Takahide <nkmrtkhd@gmail.com>
2011-11-23 11:25:28 +0900

    modify linux-compile.sh

--- compile.sh
+++ compile.sh
@@ -10,5 +10,5 @@ javah -classpath . jspglib.JSpgLib && \
 gcc -fPIC -g -c -I/System/Library/Frameworks/JavaVM.framework/Headers -I/Users/nakamura/myLocal/spglib-1.0.9/include/spglib interfaceJSpgLib.c && \
 gcc -shared  -L/Users/nakamura/myLocal/spglib-1.0.9/lib/ -lsymspg -lm \
 -o libinterfaceJSpgLib.jnilib interfaceJSpgLib.o && \
-java -classpath . jspglib.JSpgLib
+java -classpath . jspglib.JSpgLib &&\
 ant jar
--- linux-compile.sh
+++ linux-compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+tar xzvf spglib-1.0.9.tar.gz &&\
+cd spglib-1.0.9 &&\
+./configure --prefix=$HOME/myLocal/spglib-1.0.9 &&\
+make install &&\
+cd ../ &&\
 javac jspglib/JSpgLib.java && \
 javah -classpath . jspglib.JSpgLib && \
 gcc -fPIC -g -c -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/include/linux/ -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/include/ -I/home/nakamura/myLocal/spglib-1.0.9/include/spglib interfaceJSpgLib.c && \
-gcc -shared  -L/home/nakamura/myLocal/spglib-1.0.9/lib/ -lsymspg -lm -o libinterfaceJSpgLib.so interfaceJSpgLib.o && \
-java -classpath . jspglib.JSpgLib
+gcc -shared  -L$HOME/myLocal/spglib-1.0.9/lib/ -lsymspg -lm -o libinterfaceJSpgLib.so interfaceJSpgLib.o && \
+java -classpath . jspglib.JSpgLib &&\
+ant jar
