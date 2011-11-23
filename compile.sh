d691334226ea5325167c80ca327071fd1c18a569
NAKAMURA Takahide <nkmrtkhd@gmail.com>
2011-11-23 11:23:31 +0900

    add spglib-1.0.9

--- compile.sh
+++ compile.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+tar xzvf spglib-1.0.9.tar.gz &&\
+cd spglib-1.0.9 &&\
+./configure --prefix=$HOME/myLocal/spglib-1.0.9 &&\
+make install &&\
+cd ../ &&\
 javac jspglib/JSpgLib.java && \
 javah -classpath . jspglib.JSpgLib && \
 gcc -fPIC -g -c -I/System/Library/Frameworks/JavaVM.framework/Headers -I/Users/nakamura/myLocal/spglib-1.0.9/include/spglib interfaceJSpgLib.c && \
 gcc -shared  -L/Users/nakamura/myLocal/spglib-1.0.9/lib/ -lsymspg -lm \
 -o libinterfaceJSpgLib.jnilib interfaceJSpgLib.o && \
 java -classpath . jspglib.JSpgLib
+ant jar
